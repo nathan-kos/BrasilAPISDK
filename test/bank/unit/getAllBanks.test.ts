7e80c90c8a9642d061a0b622ebf095454823dd63
nathan ks <nathanks0210@gmail.com>
2025-06-26 21:44:18 -0300

    feat: add unit tests for getAllBanks

---
+++ test/bank/unit/getAllBanks.test.ts
@@ -0,0 +1,42 @@
+import { getAllBanks } from '@src/modules/bank';
+import { Bank } from '@src/modules/bank/entities/Bank';
+import { ServerError } from '@src/shared/exceptions/ServerError';
+import { describe, expect, it, vi } from 'vitest';
+
+describe('getAllBanks - unit', () => {
+  it('deve retornar os dados corretamente quando o fetch for bem-sucedido', async () => {
+    const mockedBanks: Bank[] = [
+      {
+        ispb: '12345678',
+        name: 'Banco Teste',
+        code: 999,
+        fullName: 'Banco de Teste S.A.',
+      },
+    ];
+
+    const mockFetch = vi.fn().mockResolvedValue({
+      ok: true,
+      json: () => mockedBanks,
+    });
+
+    global.fetch = mockFetch;
+
+    const result = await getAllBanks();
+    expect(result).toEqual(mockedBanks);
+    expect(mockFetch).toHaveBeenCalledTimes(1);
+    expect(mockFetch).toHaveBeenCalledWith(expect.stringContaining('/banks/v1'));
+  });
+
+  it('deve lançar erro se o fetch retornar status não-ok', async () => {
+    const mockFetch = vi.fn().mockResolvedValue({
+      ok: false,
+      status: 500,
+      statusText: 'Erro interno',
+      json: () => ({ message: 'Erro interno' }),
+    });
+
+    global.fetch = mockFetch;
+
+    await expect(getAllBanks()).rejects.toBeInstanceOf(ServerError);
+  });
+});
