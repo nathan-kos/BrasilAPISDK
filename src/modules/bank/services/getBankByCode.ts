15cbb9c35be259fb6117f88bc384e134f9f699aa
nathan ks <nathanks0210@gmail.com>
2025-06-26 21:35:18 -0300

    feat: handle unexpected errors in getBankByCode

--- src/modules/bank/services/getBankByCode.ts
+++ src/modules/bank/services/getBankByCode.ts
@@ -1,19 +1,30 @@
 import { getBaseUrl } from '@src/shared/config/ApiConfig';
+import { AppError } from '@src/shared/exceptions/AppError';
 import { handleResponseError } from '@src/shared/exceptions/HandlerResponseError';
+import { ServerError } from '@src/shared/exceptions/ServerError';
 import { Bank } from '../entities/Bank';
 
 async function getBankByCode(bankCode: string): Promise<Bank> {
   const url = `${getBaseUrl()}/banks/v1/${bankCode}`;
 
-  const response = await fetch(url);
+  try {
+    const response = await fetch(url);
 
-  if (!response.ok) {
-    await handleResponseError(response);
+    if (!response.ok) {
+      await handleResponseError(response);
+    }
+
+    const bankData = (await response.json()) as Bank;
+
+    return bankData;
+  } catch (error) {
+    if (error instanceof AppError) {
+      throw error;
+    }
+    {
+      throw new ServerError('Erro ao buscar o banco pelo código');
+    }
   }
-
-  const bankData = (await response.json()) as Bank;
-
-  return bankData;
 }
 
 export { getBankByCode };
