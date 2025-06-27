afc3200b143e3892afb537547ada4ad08eb7061c
nathan ks <nathanks0210@gmail.com>
2025-06-26 21:34:46 -0300

    feat: handle unexpected errors in getAllBanks

--- src/modules/bank/services/getAllBanks.ts
+++ src/modules/bank/services/getAllBanks.ts
@@ -1,19 +1,29 @@
 import { getBaseUrl } from '@src/shared/config/ApiConfig';
+import { AppError } from '@src/shared/exceptions/AppError';
 import { handleResponseError } from '@src/shared/exceptions/HandlerResponseError';
+import { ServerError } from '@src/shared/exceptions/ServerError';
 import { Bank } from '../entities/Bank';
 
 async function getAllBanks(): Promise<Bank[]> {
   const url = `${getBaseUrl()}/banks/v1`;
 
-  const response = await fetch(url);
+  try {
+    const response = await fetch(url);
 
-  if (!response.ok) {
-    await handleResponseError(response);
+    if (!response.ok) {
+      await handleResponseError(response);
+    }
+
+    const banksData = (await response.json()) as Bank[];
+
+    return banksData;
+  } catch (error) {
+    if (error instanceof AppError) {
+      throw error;
+    } else {
+      throw new ServerError('Erro ao buscar todos os bancos');
+    }
   }
-
-  const banksData = (await response.json()) as Bank[];
-
-  return banksData;
 }
 
 export { getAllBanks };
