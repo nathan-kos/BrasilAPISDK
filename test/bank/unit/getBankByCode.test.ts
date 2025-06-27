83c0c4b32566a97cdc601805a61973b086254450
nathan ks <nathanks0210@gmail.com>
2025-06-26 21:45:08 -0300

    feat: add unit tests for getBankByCode

---
+++ test/bank/unit/getBankByCode.test.ts
@@ -0,0 +1,52 @@
+import { Bank, getBankByCode } from '@src/index';
+import { ServerError } from '@src/shared/exceptions/ServerError';
+import { describe, expect, it, vi } from 'vitest';
+
+describe('getBankByCode', () => {
+  it('deve retornar os dados corretamente quando o fetch for bem-sucedido', async () => {
+    const mockBank: Bank = {
+      ispb: '12345678',
+      name: 'Banco Teste',
+      code: 999,
+      fullName: 'Banco de Teste S.A.',
+    };
+
+    const mockFetch = vi.fn().mockResolvedValue({
+      ok: true,
+      json: () => mockBank,
+    });
+
+    global.fetch = mockFetch;
+
+    const result = await getBankByCode('999');
+
+    expect(result).toEqual(mockBank);
+    expect(mockFetch).toHaveBeenCalledTimes(1);
+    expect(mockFetch).toHaveBeenCalledWith(expect.stringContaining('/banks/v1/999'));
+  });
+
+  it('deve lançar erro se o código do banco não for encontrado', async () => {
+    const mockFetch = vi.fn().mockResolvedValue({
+      ok: false,
+      status: 404,
+      statusText: 'Not Found',
+      json: () => ({ message: 'Banco não encontrado' }),
+    });
+
+    global.fetch = mockFetch;
+
+    await expect(getBankByCode('NKS')).rejects.toThrow('Banco não encontrado');
+    expect(mockFetch).toHaveBeenCalledTimes(1);
+    expect(mockFetch).toHaveBeenCalledWith(expect.stringContaining('/banks/v1/NKS'));
+  });
+
+  it('deve lançar erro se ocorrer um problema na requisição', async () => {
+    const mockFetch = vi.fn().mockRejectedValue(new ServerError());
+
+    global.fetch = mockFetch;
+
+    await expect(getBankByCode('999')).rejects.toBeInstanceOf(ServerError);
+    expect(mockFetch).toHaveBeenCalledTimes(1);
+    expect(mockFetch).toHaveBeenCalledWith(expect.stringContaining('/banks/v1/999'));
+  });
+});
