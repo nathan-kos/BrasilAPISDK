e8caf1431bff78039637abce0abf863dcc367fa0
nathan ks <nathanks0210@gmail.com>
2025-07-03 00:10:34 -0300

    feat: add abort controller on getAllBanks service

--- src/modules/bank/services/getAllBanks.ts
+++ src/modules/bank/services/getAllBanks.ts
@@ -1,4 +1,4 @@
-import { getBaseUrl } from '@src/shared/config/ApiConfig';
+import { getBaseUrl, getTimeout } from '@src/shared/config/ApiConfig';
 import { AppError } from '@src/shared/exceptions/AppError';
 import { handleResponseError } from '@src/shared/exceptions/HandlerResponseError';
 import { ServerError } from '@src/shared/exceptions/ServerError';
@@ -8,7 +8,13 @@ async function getAllBanks(): Promise<Bank[]> {
   const url = `${getBaseUrl()}/banks/v1`;
 
   try {
-    const response = await fetch(url);
+    const controller = new AbortController();
+    const timeoutId = setTimeout(() => controller.abort(), getTimeout());
+
+    const response = await fetch(url, {
+      signal: controller.signal,
+    });
+    clearTimeout(timeoutId);
 
     if (!response.ok) {
       await handleResponseError(response);
