a4916b6f18e282c9cdabe35eb574dd53b83b8f79
nathan ks <nathanks0210@gmail.com>
2025-07-29 19:40:40 -0300

    feat: currenty symbol enum

---
+++ src/modules/currency/entities/CurrencySymbol.ts
@@ -0,0 +1,14 @@
+enum CurrencySymbol {
+  AUD = 'AUD',
+  CAD = 'CAD',
+  CHF = 'CHF',
+  DKK = 'DKK',
+  EUR = 'EUR',
+  GBP = 'GBP',
+  JPY = 'JPY',
+  NOK = 'NOK',
+  SEK = 'SEK',
+  USD = 'USD',
+}
+
+export { CurrencySymbol };
