c16b1d0ecc2c80144d9e34f52ca29e7ba09d9cca
nathan ks <nathanks0210@gmail.com>
2025-11-27 23:44:42 -0300

    feat: vehicle models on fipe index

--- src/modules/fipe/index.ts
+++ src/modules/fipe/index.ts
@@ -1,5 +1,6 @@
 export * from './entities/fipeBrand';
 export * from './entities/vehicleType';
 export * from './service/getBrandByVehicleType';
+export * from './service/getBrandsVehicleModel';
 export * from './service/getFipeTable';
 
