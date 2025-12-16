d5f8c90b6d856c2abf399418e144cfa3afbb8b33
nathan ks <nathanks0210@gmail.com>
2025-12-16 07:57:21 -0300

    feat: ncm entity

---
+++ src/modules/ncm/entities/ncmInfo.ts
@@ -0,0 +1,11 @@
+interface NcmInfo {
+  code: string;
+  description: string;
+  startDate: Date;
+  endDate?: Date;
+  actType: string;
+  actNumber: string;
+  actYear: number;
+}
+
+export { NcmInfo };
